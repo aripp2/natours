aad963b40c1b1f588a6f0a9c86c2ebe5935f027f
Amy Rippeto <amyrippeto@gmail.com>
2020-03-21 08:48:04 -0600

    Add patch route handler

--- app.js
+++ app.js
@@ -107,6 +107,25 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+// Patch - Update a single tour by id
+
+app.patch(`/api/v1/tours/:id`, (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<UPDATED TOUR HERE>'
+    }
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
