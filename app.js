1a5768012dec76e9003d469554cc20aacca4e89c
Amy Rippeto <amyrippeto@gmail.com>
2020-03-26 11:16:51 -0600

    Refactor route handlers

--- app.js
+++ app.js
@@ -30,37 +30,57 @@ const tours = JSON.parse(
 // Route Handlers
 
 // Get All Tours
-app.get('/api/v1/tours', (req, resp) => {
-  resp.status(200).json({
+const getAllTours = (req, res) => {
+  res.status(200).json({
     status: 'success',
     // number of results if sending an array with multiple objects
     results: tours.length,
     data: {
-      // do not have to specify if the key and value have the same name
       tours
     }
   });
-});
+};
 
 // Response Example
 /*
-  {
-    "status": "success",
-    "results": 9,
-    "data" : {
-      "tours":[
-        {  all tour objects }
-      ]
-    }
+{
+  "status": "success",
+  "results": 9,
+  "data" : {
+    "tours":[
+      {  all tour objects }
+    ]
   }
+}
 */
 
-// Create New Tour
-//only change the method, not route
+// Get One Tour by ID
+// define the variable in the route with a /:varName referred to the req as params
+// can change multiple variables /:var1/:var2/:var3
+// the url is required to have all params to be successful
+// Optional Params so we do not have to specify with a ? after /:var1/:var2/:var3?
+const getTour = (req, res) => {
+  // Automatically convert the string of the request id to a number
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+  // Error handling for not found
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  resp.status(200).json({
+    status: 'success',
+    data: {
+      tour
+    }
+  });
+};
 
-app.post('/api/v1/tours', (req, res) => {
-  // sent data
-  // console.log(req.body);
+// Create New Tour - only change the method, not route
+const createTour = (req, res) => {
+  // sent data => req.body
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -77,39 +97,10 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-  // res.send('Done 😆');
-});
-
-// Get One Tour by ID
-// define the variable in the route with a /:varName referred to the req as params
-// can change multiple variables /:var1/:var2/:var3
-// the url is required to have all params to be successful
-// Optional Params so we do not have to specify with a ? after /:var1/:var2/:var3?
-
-app.get('/api/v1/tours/:id', (req, res) => {
-  console.log(req.params);
-  // Automatically convert the string of the request id to a number
-  const id = req.params.id * 1;
-  const tour = tours.find(el => el.id === id);
-  // Error handling for not found
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-
-  resp.status(200).json({
-    status: 'success',
-    data: {
-      tour
-    }
-  });
-});
+};
 
 // Patch - Update a single tour by id
-
-app.patch(`/api/v1/tours/:id`, (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find(el => el.id === id);
   if (!tour) {
@@ -121,13 +112,13 @@ app.patch(`/api/v1/tours/:id`, (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<UPDATED TOUR HERE>'
+      tour: '<UPDATED TOUR HERE...>'
     }
   });
-});
+};
 
 // Delete
-app.delete('/api/v1/tours/:id', (req, res) => {
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -139,7 +130,24 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null
   });
-});
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch(`/api/v1/tours/:id`, updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
+
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
